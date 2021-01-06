f061bad0f168e48a6f0d2a242de99cb003bb1e5c
Anıl Can SAATCI <anlcan.saatci@gmail.com>
2021-01-06 20:11:10 +0300

    Update bitproje.ino

--- bitproje.ino
+++ bitproje.ino
@@ -1,29 +1,68 @@
 
 #define BLYNK_PRINT Serial
-
-// WiFi iletişimi için gerekli kütüphaneler
+#include <TinyGPS.h>
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
-// Blink server'ında bu sensöre özel tanınma bileti
+TinyGPS gpst;
+WiFiClient client;
+
 char auth[] = "8af83da6bc31489b9eec51b8f0bc3890";
+char* ssid[] = {"Redmi Note 9","Tilgin-ku5QAdxxSxK6"};
+char* pass[] = {"anilharikadir","dyDyuBt9hTbkR"};
+int check_out = 0;
+int check_in = 0;
+boolean evde_miyim = true;
+long telefon = millis();
+bool haber = false;
+long last_seen;
+long first_timer = true;
+int length;
 
-// Cihazın bağlanıcağı WiFi'ın bilgileri
-char ssid[] = "Tilgin-ku5QAdxxSxK6";
-char pass[] = "dyDyuBt9hTbkR";
-
+BLYNK_READ(V1) // Widget in the app READs Virtal Pin V5 with the certain frequency
+{
+  // This command writes Arduino's uptime in seconds to Virtual Pin V5
+  Blynk.virtualWrite(1, map(analogRead(A0),0,1023,0,15));
+}
 void setup()
 {
-  // Serial Port'u başlatıyorum
-  Serial.begin(9600);
-// İnternet ve Server bağlantısını gerçekleştiriyorum
-  Blynk.begin(auth, ssid, pass);
+  // Debug console
+
+MultyWiFiBlynkBegin();
+  
 
 }
 
 void loop()
 {
-  //Server'a bağlantı kuruyorum.
-  //Server'dan internet aracılığı ile bu cihazın analog inputuna erişip, işlenicek
   Blynk.run();
+
+
+  
+}
+
+
+void MultyWiFiBlynkBegin() {
+  int ssid_count=0;
+  int ssid_mas_size = sizeof(ssid) / sizeof(ssid[0]);
+  do {
+    Serial.println("Trying to connect to wi-fi " + String(ssid[ssid_count]));
+    WiFi.begin(ssid[ssid_count], pass[ssid_count]);    
+    int WiFi_timeout_count=0;
+    while (WiFi.status() != WL_CONNECTED && WiFi_timeout_count<50) { //waiting 10 sec
+      delay(200);
+      Serial.print(".");
+      ++WiFi_timeout_count;
+    }
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("Connected to WiFi! Now I will check the connection to the Blynk server");
+      Blynk.config(auth);
+      Blynk.connect(5000); //waiting 5 sec
+    }
+    ++ssid_count; 
+  }
+  while (!Blynk.connected() && ssid_count<ssid_mas_size);
+  if (!Blynk.connected() && ssid_count==ssid_mas_size) {
+    Serial.println("I could not connect to blynk =( Ignore and move on. but still I will try to connect to wi-fi " + String(ssid[ssid_count-1]));
+  }
 }
